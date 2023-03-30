181f9d14e603c214bd57f83d765ea9b4f17711a0
Cfnatic7 <78828448+Cfnatic7@users.noreply.github.com>
2023-03-30 21:47:24 +0200

    feat: init

---
+++ src/test/java/com/example/conferences/conferencesapp/ConferencesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conferences.conferencesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferencesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
